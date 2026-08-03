468fcc8186951bed784688c33fc4455e57c60e28
Mike Hahn <hahnbytes@gmail.com>
2026-08-02 21:05:22 -0400

    Add files via upload

--- src/main/java/iconst/NodeCellTyp.java
+++ src/main/java/iconst/NodeCellTyp.java
@@ -2,7 +2,7 @@ package iconst;
 
 public enum NodeCellTyp {
 	NULL, BOOLEAN, INT, LONG, FLOAT, STRING, ID, FUNC,
-	PTR, KWD, LOCVAR, FLDVAR;
+	PTR, KWD, LOCVAR, FLDVAR, GLBVAR;
 	
 	public static final NodeCellTyp values[] = values();
 }
